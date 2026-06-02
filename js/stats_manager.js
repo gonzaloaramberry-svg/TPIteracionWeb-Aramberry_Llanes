889c3048f220952f2a7208de041a02e83bcbca34
koala <gonzaloaramberry@gmail.com>
2026-06-02 20:48:58 -0300

    Implementacion de conexion WebSocket

---
+++ js/stats_manager.js
@@ -0,0 +1,72 @@
+function StatsManager() {
+
+  this.statsKey = "gameStats";
+
+  this.stats = JSON.parse(localStorage.getItem(this.statsKey)) || {
+    partidas: 0,
+    movimientos: 0,
+    victorias: 0,
+    derrotas: 0,
+    mejorPuntaje: 0,
+    combinaciones: 0
+  };
+    this.socket = new WebSocket(
+    "wss://gamehubmanager.azurewebsites.net/ws"
+  );
+
+  this.socket.onopen = function() {
+    console.log("WebSocket conectado");
+  };
+
+  this.socket.onerror = function(error) {
+    console.error("Error WebSocket:", error);
+  };
+
+  this.socket.onclose = function() {
+    console.log("WebSocket cerrado");
+  };
+}
+
+// Guardar estadísticas
+StatsManager.prototype.guardar = function () {
+  localStorage.setItem(this.statsKey, JSON.stringify(this.stats));
+};
+
+// Registrar eventos
+StatsManager.prototype.registrarEvento = function (evento, valor) {
+
+  switch(evento) {
+
+    case "partida":
+      this.stats.partidas++;
+      break;
+
+    case "movimiento":
+      this.stats.movimientos++;
+      break;
+
+    case "victoria":
+      this.stats.victorias++;
+      break;
+
+    case "derrota":
+      this.stats.derrotas++;
+      break;
+
+    case "combinacion":
+      this.stats.combinaciones++;
+      break;
+
+    case "puntaje":
+      if (valor > this.stats.mejorPuntaje) {
+        this.stats.mejorPuntaje = valor;
+      }
+      break;
+    
+  }
+
+  this.guardar();
+
+  console.log("Evento registrado:", evento);
+  console.log(this.stats);
+};
