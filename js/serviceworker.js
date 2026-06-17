981968c2bc9f3ebf8c3e503a26e484b1bb0fe74a
koala <gonzaloaramberry@gmail.com>
2026-06-17 17:40:48 -0300

    Creacion y registro del Serviceworker

---
+++ js/serviceworker.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = "2048-cache-v1";
+
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/style/main.css",
+  "/js/application.js",
+  "/js/game_manager.js",
+  "/js/stats_manager.js"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
