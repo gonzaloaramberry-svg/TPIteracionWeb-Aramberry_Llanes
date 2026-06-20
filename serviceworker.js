15abf0e0271ec347f78792d2d881dcfbdd48503e
koala <gonzaloaramberry@gmail.com>
2026-06-20 20:00:43 -0300

    .

--- serviceworker.js
+++ serviceworker.js
@@ -1,12 +1,13 @@
-const CACHE_NAME = "2048-cache-v1";
+const CACHE_NAME = "2048-cache-v2";
 
 const urlsToCache = [
-  "./",                 
-  "./index.html",        
-  "./style/main.css",   
+  "./",
+  "./index.html",
+  "./style/main.css",
   "./js/application.js",
   "./js/game_manager.js",
-  "./js/stats_manager.js"
+  "./js/stats_manager.js",
+  "./js/gyro.js"
 ];
 
 self.addEventListener("install", event => {
@@ -14,6 +15,18 @@ self.addEventListener("install", event => {
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
   );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
